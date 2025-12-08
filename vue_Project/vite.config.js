243a694bdcc0124e42504ef995d7047fc63dbd7b
oliverschwall03-art <oliver.schwall03@gmail.com>
2025-12-08 15:45:12 +0100

    Update vite.config.js

--- vue_Project/vite.config.js
+++ vue_Project/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue_Project/',
+  base: '/Software-Projektseminar/tree/main/vue_Project',
   plugins: [
     vue(),
     vueDevTools(),
